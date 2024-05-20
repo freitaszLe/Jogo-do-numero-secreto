45e2c7ba8f73f7f005a9a51d06d568c4972327d8
Leticia Freitas <le.freitas712@gmail.com>
2024-05-20 11:49:43 -0400

    limite 10

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 let listaNumerosorteados = [];
-let numeroLimite = 8;
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
